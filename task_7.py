1baf89cba4d9d503928951392ace519f1a2ada5a
Maksimkurilenko <maksimkurylenka@gmail.com>
2025-03-22 18:54:11 +0100

    task_7

---
+++ task_7.py
@@ -0,0 +1,5 @@
+time_seconds = int(input("Please enter time in seconds: "))
+minutes = time_seconds//60
+time_seconds %= 60
+seconds = time_seconds
+print("Your time in minutes: ",minutes,"minutes",seconds,"seconds")
