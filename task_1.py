ad130c644ce0597ba806689fe613e58e48c6a9cd
Maksimkurilenko <maksimkurylenka@gmail.com>
2025-03-22 18:45:32 +0100

    task_1

---
+++ task_1.py
@@ -0,0 +1,5 @@
+user_last_name = input ("Last name: ")
+user_first_name = input ("First name: ")
+user_middle_name = input ("Middle name: ")
+str = ("Your initials are: " + (user_last_name + " " + user_first_name[0] + "." + user_middle_name[0]+ "."))
+print(str)
